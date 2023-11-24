1bf15e9f2d9290448bf663d89be77c33ec0c38e7
Diogo Laureano <diogolaureano.sousa@gmail.com>
2023-11-23 22:23:38 -0300

    Criando e instanciando uma classe

--- Program.cs
+++ Program.cs
@@ -1,16 +1,28 @@
-﻿
-//Declarações de variáveis e seus tipos.
-string apresentacao = "Olá, seja bem vindo!";
-int quantidade = 1;
-double altura = 1.80;
-decimal preco = 1.80M;
-bool condicao = true;
+﻿//Namespace caminho para encontrar a classe
+using C_.Models;
 
-Console.WriteLine(apresentacao);
-Console.WriteLine("Valor da variavel quantidade " + quantidade);
-Console.WriteLine("Valor da variavel altua " + altura.ToString("0.00"));
-Console.WriteLine("Valor da variavel preco " + preco);
-Console.WriteLine("Valor da variavel condicao " + condicao);
+
+//Declrando e instanciando a classe pessoa
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Diogo";
+pessoa1.Idade = 33;
+pessoa1.Apresentar();
+
+
+
+//Declarações de variáveis e seus tipos.
+// string apresentacao = "Olá, seja bem vindo!";
+// int quantidade = 1;
+// double altura = 1.80;
+// decimal preco = 1.80M;
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variavel quantidade " + quantidade);
+// Console.WriteLine("Valor da variavel altua " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variavel preco " + preco);
+// Console.WriteLine("Valor da variavel condicao " + condicao);
 
 //Classe para mensagem
 //Console.WriteLine("Hello, World!");
